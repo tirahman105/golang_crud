b580455d87a69a82fcc303f5203f8bf421a8e947
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2022-05-19 03:58:24 +0600

    golang template

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	fmt.Println(" Local server is running on port 8888")
+	http.HandleFunc("/", home)
+	// http.HandleFunc("/add", add)
+	// http.HandleFunc("/dashboard", dashboard)
+	// http.HandleFunc("/mlist", mlist)
+	//built in file server
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":8888", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+	// fmt.Fprint(w, `welcome`)
+}
+
+// func add(w http.ResponseWriter, r *http.Request) {
+// 	ptmp, err := template.ParseFiles("template/base.gohtml")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+
+// 	ptmp, err = template.ParseFiles("wpage/add.gohtml")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	ptmp.Execute(w, nil)
+// 	// fmt.Fprint(w, `welcome`)
+// }
+
+// func dashboard(w http.ResponseWriter, r *http.Request) {
+// 	ptmp, err := template.ParseFiles("template/base.gohtml")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	ptmp.Execute(w, nil)
+// 	// fmt.Fprint(w, `welcome`)
+// }
+
+// func mlist(w http.ResponseWriter, r *http.Request) {
+// 	ptmp, err := template.ParseFiles("template/base.gohtml")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	ptmp.Execute(w, nil)
+// 	// fmt.Fprint(w, `welcome`)
+// }
